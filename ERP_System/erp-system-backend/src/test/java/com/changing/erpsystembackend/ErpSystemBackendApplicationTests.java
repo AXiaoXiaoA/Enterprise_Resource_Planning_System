b22f78533a8d7a6706310f6aa5b3cdca1258aab8
ChangeeShaw <x1050692380@gmail.com>
2024-05-06 02:54:48 +0800

    Initial backend

---
+++ ERP_System/erp-system-backend/src/test/java/com/changing/erpsystembackend/ErpSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.changing.erpsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
